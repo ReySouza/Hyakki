12491fea50f1057b39fa51a8bf4c449057c4e81c
ReySouza <122836752+ReySouza@users.noreply.github.com>
2023-02-12 15:37:07 -0300

    Update map.js

--- map.js
+++ map.js
@@ -5,3 +5,11 @@ var map = L.map('map').setView([35.68, 139.69], 5);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// Add a Google satellite map as a tile layer to the map
+var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains:['mt0','mt1','mt2','mt3']
+});
+
+googleSat.addTo(map);
