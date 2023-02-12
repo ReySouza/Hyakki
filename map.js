06f8afe7498c4a18d402d94dc13030fa47952aae
ReySouza <122836752+ReySouza@users.noreply.github.com>
2023-02-12 15:26:24 -0300

    Update map.js

--- map.js
+++ map.js
@@ -1,26 +1,7 @@
-var map;
-var roadmapView = true;
+// Create a map object and set the view to the center of Japan
+var map = L.map('map').setView([35.68, 139.69], 5);
 
-function initMap() {
-  map = L.map("map").setView([37.0902, 138.5], 5);
-
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-  }).addTo(map);
-
-  var toggleBtn = document.createElement("button");
-  toggleBtn.innerHTML = "Toggle View";
-  toggleBtn.style.position = "absolute";
-  toggleBtn.style.top = "10px";
-  toggleBtn.style.right = "10px";
-  toggleBtn.addEventListener("click", function() {
-    if (roadmapView) {
-      // Add code to switch to satellite view here
-      roadmapView = false;
-    } else {
-      // Add code to switch back to roadmap view here
-      roadmapView = true;
-    }
-  });
-  document.body.appendChild(toggleBtn);
-}
+// Add a tile layer to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
