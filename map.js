a1c718fda55cab92bcbd3eaafac20dde28f28fde
ReySouza <122836752+ReySouza@users.noreply.github.com>
2023-02-12 15:40:05 -0300

    Update map.js

--- map.js
+++ map.js
@@ -2,9 +2,9 @@
 var map = L.map('map').setView([35.68, 139.69], 5);
 
 // Add a tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+});
 
 // Add a Google satellite map as a tile layer to the map
 var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
@@ -12,4 +12,25 @@ var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
   subdomains:['mt0','mt1','mt2','mt3']
 });
 
-googleSat.addTo(map);
+// Add the OpenStreetMap layer to the map by default
+osm.addTo(map);
+
+// Function to toggle between the two maps
+function toggleMaps() {
+  if (map.hasLayer(osm)) {
+    map.removeLayer(osm);
+    map.addLayer(googleSat);
+  } else {
+    map.removeLayer(googleSat);
+    map.addLayer(osm);
+  }
+}
+
+// Add a button to the map
+var button = L.control({position: 'topright'});
+button.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'leaflet-control-layers leaflet-control');
+  div.innerHTML = '<button onclick="toggleMaps()">Toggle Maps</button>';
+  return div;
+};
+button.addTo(map);
