46a0438f6c3e4b598a934de5c905264226ea964b
ReySouza <122836752+ReySouza@users.noreply.github.com>
2023-02-14 19:36:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+const data = {
+      expertise: [
+         name:
+         cost: 
+         description:
+      ]
+   }
+
+data.expertise.map((expertise, index) => {
+   addExpertiseToTable(expertise, index)
+})
+
+$('#addExpertise').click(function() {
+   openModal('#addExpertiseModal')
+})
+
+$('#addExpertiseForm').submit(function (event) {
+  var expertiseCost = ''
+
+  if ($('#expertiseCost').val() == 1) {
+    expertiseCost = 1
+  } else if ($('#expertiseCost').val() == 2) {
+    expertiseCost = 2
+  } else if ($('#expertiseCost').val() == 3) {
+    expertiseCost = 3
+  }
+
+  const expertise = {
+    name: $('#expertiseName').val(),
+    cost: $('#expertiseCost').val(),
+    description: $('#expertiseDescription').val(),
+  }
+
+  data.expertise.push(expertise)
+  const id = data.expertise.length - 1
+  addExpertiseToTable(expertise, id)
+
+  closeModal('#addExpertiseModal')
+  event.preventDefault()
+})
+
+function addExpertiseToTable(expertise, id) {
+  const newexpertise = $(`<tr id="expertise_${id}">
+        <td>
+            <button onclick="deleteExpertise(${id})">
+                <i class="fa fa-trash-o trashcan"></i>
+            </button>
+            ${expertise.name}
+        </td>
+        <td>${expertise.cost}</td>
+        <td>${expertise.description}</td>
+    </tr>`)
+  $('table#expertise').append(newexpertise)
+}
+
+function deleteExpertise(id) {
+  $(`tr#${id}`).remove()
+}
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
