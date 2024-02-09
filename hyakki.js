19b330fd08b152a8f216590d7692254f997291f5
Reynaldo Souza de Carvalho <122836752+ReySouza@users.noreply.github.com>
2024-02-08 21:36:46 -0300

    Update hyakki.js

--- hyakki.js
+++ hyakki.js
@@ -7,7 +7,7 @@ function checkPassword(event) {
                     window.location.href = "https://reysouza.github.io/Hyakki/karasawa.html";
                 } else if (password === "731"){
                     window.location.href = "https://reysouza.github.io/Hyakki/731.html";
-                } else if (password === "lunaflama"){
+                } else if (password === "lunachama"){
                     window.location.href = "https://reysouza.github.io/Hyakki/lunaflama.html";
             } else {
                   window.location.href = "https://reysouza.github.io/Hyakki/bestiary.html";
